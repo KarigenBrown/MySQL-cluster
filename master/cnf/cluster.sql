07e56230dbfdf28b8ba6879711d14bcc8f10affb
KarigenB <1837633996@qq.com>
2022-10-05 11:10:40 +0800

    build: 主体

--- master/cnf/cluster.sql
+++ master/cnf/cluster.sql
@@ -1,4 +1,3 @@
-CREATE USER reader IDENTIFIED BY 'reader';
 GRANT REPLICATION SLAVE ON *.* to 'reader'@'%';
 ALTER USER 'reader'@'%' IDENTIFIED WITH mysql_native_password BY 'reader';
 FLUSH PRIVILEGES;
