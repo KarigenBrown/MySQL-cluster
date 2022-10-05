e10131da70f73477400c7b7f8d5d3843fec840b1
KarigenB <1837633996@qq.com>
2022-10-06 00:02:55 +0800

    build: nginx

--- slave-2/cnf/cluster.sql
+++
@@ -1,3 +0,0 @@
-change master to master_host='mysql-master',master_user='reader',master_password='reader',master_log_file='mysql-bin.000007',master_log_pos=546;
-reset slave;
-start slave;
--- slave/sql/cluster.sql
+++ slave/sql/cluster.sql
@@ -1,3 +1,5 @@
+GRANT ALL PRIVILEGES ON *.* TO 'reader'@'%';
+flush privileges;
 change master to master_host="mysql-master",master_user='reader',master_password='reader',master_log_file='mysql-bin.000010',master_log_pos=1503;
 reset slave;
 start slave;
