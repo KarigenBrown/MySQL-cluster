6e5c9e2f0ac47c1139f4d70dd1ae4c08fe7a5aa3
KarigenB <1837633996@qq.com>
2022-10-05 10:15:08 +0800

    build: 主体

--- cluster.sh
+++ cluster.sh
@@ -1,4 +1,6 @@
 #/bin/bash
-docker run -it -d -p 3307:3306 --name mysql-master -v ./master/cnf:/etc/mysql/conf.d -v ./master/data:/var/lib/mysql -e MYSQL_ROOT_PASSWORD=123456 --network mynet mysql:8
-docker run -it -d -p 3308:3306 --name mysql-slave-1 -v ./slave-1/cnf:/etc/mysql/conf.d -v ./slave-1/data:/var/lib/mysql -e MYSQL_ROOT_PASSWORD=123456 --network mynet mysql:8
-docker run -it -d -p 3309:3306 --name mysql-slave-2 -v ./slave-2/cnf:/etc/mysql/conf.d -v ./slave-2/data:/var/lib/mysql -e MYSQL_ROOT_PASSWORD=123456 --network mynet mysql:8
+docker network create mysql-cluster --subnet 192.168.127.0/24
+
+docker run -it -d -p 3307:3306 --name mysql-master -v `pwd`/master/cnf:/etc/mysql/conf.d -v `pwd`/master/data:/var/lib/mysql -e MYSQL_ROOT_PASSWORD=123456 --network mysql-cluster mysql:8
+docker run -it -d -p 3308:3306 --name mysql-slave-1 -v `pwd`/slave-1/cnf:/etc/mysql/conf.d -v `pwd`/slave-1/data:/var/lib/mysql -e MYSQL_ROOT_PASSWORD=123456 --network mysql-cluster mysql:8
+docker run -it -d -p 3309:3306 --name mysql-slave-2 -v `pwd`/slave-2/cnf:/etc/mysql/conf.d -v `pwd`/slave-2/data:/var/lib/mysql -e MYSQL_ROOT_PASSWORD=123456 --network mysql-cluster mysql:8
